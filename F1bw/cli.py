f978d4a7921588ce0206ada46fc5d4f998009fd7
MChigaev <39177635+MChigaev@users.noreply.github.com>
2018-07-18 15:21:53 -0600

    Update cli.py

--- F1bw/cli.py
+++ F1bw/cli.py
@@ -1,8 +1,12 @@
 def radix_sort(values, key, step=0):
-    """Performs a radix sort.
+    """
+    Performs a radix sort.
     :param the number values to sort.
+    
     :param key: The key to use.
-    :param step: The current step. Default is 0."""
+   
+    :param step: The current step. Default is 0.
+    """
     if len(values) < 2:
         for value in values:
             yield value
@@ -16,22 +20,30 @@ def radix_sort(values, key, step=0):
 
 
 def bw_key(text, value, step):
-    """Generates a key for a character in text
+    """
+    Generates a key for a character in text
     :param text: the text to have a key generated from.
+    
     :param value: The value which is used to determine the character, in addition to the step.
-    :param step: The step, used in the radix sort."""
+    
+    :param step: The step, used in the radix sort.
+    """
     return text[(value + step) % len(text)]
 
 
 def burroughs_wheeler_custom(text):
-    """Performs the transformation.
-    @param: text the text to be transformed."""
+    """
+    Performs the transformation.
+    @param: text the text to be transformed.
+    """
     return ''.join(text[i - 1] for i in radix_sort(range(len(text)), partial(bw_key, text)))
 
 
 def transform(inp):
-    """Finds repeats and capitalizes them after transforming the string.
-    :param inp: the text to be transformed and capitalized based off of repeats."""
+    """
+    Finds repeats and capitalizes them after transforming the string.
+    :param inp: the text to be transformed and capitalized based off of repeats.
+    """
     inp = inp.lower()
     out = burroughs_wheeler_custom(inp)
     x = 0
@@ -43,9 +55,12 @@ def transform(inp):
 
 
 def inverse(inp, endchr):
-    """Performs the inverse of the transformation.
+    """
+    Performs the inverse of the transformation.
     :param inp: the text to have the inverse applied to it.
-    :param endchr: the end character that is used to figure out the original string."""
+    
+    :param endchr: the end character that is used to figure out the original string.
+    """
     inp = inp.lower()
     out = []
     x = 0
