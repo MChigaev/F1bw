63f2df5a8c7c42aa8cab5d95b6b1e8f4d70d0cb7
MChigaev <39177635+MChigaev@users.noreply.github.com>
2018-07-18 11:03:06 -0600

    Update __init__.py

--- F1bw/__init__.py
+++ F1bw/__init__.py
@@ -2,25 +2,25 @@ import click
 
 
 def radix_sort(values, key, step=0):
-    if len(values) < 2:
-        for value in values:
-            yield value
-        return
-    bins = {}
-    for value in values:
-        bins.setdefault(key(value, step), []).append(value)
-    for k in sorted(bins.keys()):
-        for r in radix_sort(bins[k], key, step + 1):
-            yield r
+	if len(values) < 2:
+		for value in values:
+			yield value
+		return
+	bins = {}
+	for value in values:
+		bins.setdefault(key(value, step), []).append(value)
+	for k in sorted(bins.keys()):
+		for r in radix_sort(bins[k], key, step + 1):
+			yield r
 
 
 def bw_key(text, value, step):
-    return text[(value + step) % len(text)]
+	return text[(value + step) % len(text)]
 
 
 def burroughs_wheeler_custom(text):
-    return ''.join(text[i - 1] for i in radix_sort(range(len(text)), partial(bw_key, text)))
-
+	return ''.join(text[i - 1] for i in radix_sort(range(len(text)), 
+partial(bw_key, text)))
 
 def transform(inp):
 	inp = inp.lower()
