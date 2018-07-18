452cedf8a6e24aaac7df3d84284ce64bdefee283
MChigaev <39177635+MChigaev@users.noreply.github.com>
2018-07-18 15:26:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,23 +24,8 @@ from setuptools.command import build_py, develop, install
 # Version restrictions and dependencies
 if sys.version_info < (3, 4, 0, 'final', 0):
     raise SystemExit("This package requires python 3.4 or higher.")
-elif sys.version_info >= (3, 6, 0, 'final', 0):
-    pass
-else:
-    pass
-from pathlib import Path  # python 3.4
 
 NAME = 'F1bw'
-C_NAME = 'F1bw'
-C_VERSION = '0.0.1'
-BINARY_NAME = C_NAME + '-' + NAME
-ENV_SCRIPT_INNAME = 'server_env.sh'
-ENV_SCRIPT_OUTNAME = NAME + '_env'
-RUN_SCRIPT_INNAME = 'server_run.py'
-RUN_SCRIPT_OUTNAME = NAME + '_run.py'
-SOURCE_PATH = Path('.') / NAME / C_NAME.lower() + ".py"
-BUILD_PATH = Path('.') / NAME / 'bin'
-DIR_MODE = 0o775
 
 #
 # Most of the setup function has been moved to setup.cfg,
@@ -50,14 +35,14 @@ DIR_MODE = 0o775
 #
 
 tests_require = [
-    #'check-manifest>=0.25',
-    #'coverage>=4.0',
-    #'isort>=4.2.2.2',
-    #'pydocstyle>=1.0.0',
-    #'pytest-cache>=1.0',
-    #'pytest-cov>=1.8.0',
-    #'pytest-pep8>=1.0.6',
-    #'pytest>=2.8.0'
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'isort>=4.2.2.2',
+    'pydocstyle>=1.0.0',
+    'pytest-cache>=1.0',
+    'pytest-cov>=1.8.0',
+    'pytest-pep8>=1.0.6',
+    'pytest>=2.8.0'
 ]
 
 extras_require = dict(docs=['Sphinx>=1.4.2'], tests=tests_require)
@@ -71,19 +56,11 @@ packages = find_packages()
 setup(
     description=__doc__,
     packages=packages,
-    setup_requires=['packaging',
-                    'setuptools>30.3.0',
+    setup_requires=['setuptools>30.3.0',
                     'setuptools-scm>1.5'
                     ],
     entry_points={
         'console_scripts': [NAME + ' = ' + NAME + '.cli:cli']
-    },
-    cmdclass={
-        #'build_cbinary': BuildCBinaryCommand,
-        #'build_py': BuildPyCommand,
-        #'develop': DevelopCommand,
-        #'install_binaries': InstallBinariesCommand,
-        #'install': InstallCommand
     },
     use_scm_version={
         'version_scheme': 'guess-next-dev',
